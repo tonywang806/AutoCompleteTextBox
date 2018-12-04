56a320e8436257403f42ce50c4ce14d45ef4d5ab
Tony Wang <tonywang806@gmail.com>
2018-12-04 13:17:53 +0900

    Add Useage
    The Usage of CustomizedTextBox

---
+++ AutoCompleteTextBoxDemo/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFeature = New AcTextBox.CustomizedTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtFeature
+        '
+        Me.txtFeature.AutoCompleteValues = Nothing
+        Me.txtFeature.Location = New System.Drawing.Point(66, 51)
+        Me.txtFeature.Name = "txtFeature"
+        Me.txtFeature.Size = New System.Drawing.Size(100, 19)
+        Me.txtFeature.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(66, 89)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(138, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "「ち」を入力してみてください。"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtFeature)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFeature As AcTextBox.CustomizedTextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ AutoCompleteTextBoxDemo/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim nameArray() As String = {"ALualuAuAuu", "al", "ＡＬ", "チェック", "ちぇっく", "ﾁｪｯｸ", "1234567890ABCDEFGﾁｪｯｸ"}
+        txtFeature.AutoCompleteValues = nameArray
+    End Sub
+End Class
